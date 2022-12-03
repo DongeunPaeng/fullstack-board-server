11a689e47ed18aa6735b314e33ec528e3777dd71
Dongeun Paeng <dongeun.paeng@gmail.com>
2022-12-03 16:32:35 +0900

    feat: 이전글 보기 기능 추가

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -8,104 +8,103 @@ const postService = new PostService();
 const postRouter = express.Router();
 
 postRouter.get("/", async (req, res) => {
-  try {
-    const queryResults = await postService.getPosts();
-    const posts = queryResults;
+    try {
+        const queryResults = await postService.getPosts();
+        const posts = queryResults;
 
-    res.status(200).json({ posts });
-  } catch (err) {
-    return res.status(500).json({ message: "something went wrong! sorry." });
-  }
+        res.status(200).json({ posts });
+    } catch (err) {
+        return res.status(500).json({ message: "something went wrong! sorry." });
+    }
 });
 
 postRouter.post("/draft", verifyToken, async (req, res) => {
-  try {
-    const queryResults = await postService.getDrafts();
-    const posts = queryResults;
+    try {
+        const queryResults = await postService.getDrafts();
+        const posts = queryResults;
 
-    res.status(200).json({ posts });
-  } catch (err) {
-    return res.status(500).json({ message: "something went wrong! sorry." });
-  }
+        res.status(200).json({ posts });
+    } catch (err) {
+        return res.status(500).json({ message: "something went wrong! sorry." });
+    }
 });
 
 postRouter.get("/:id", async (req, res) => {
-  const {
-    params: { id },
-  } = req;
+    const {
+        params: { id },
+    } = req;
 
-  const postId = id;
-
-  try {
-    const queryResults = await postService.getPost(postId);
-    const post = queryResults[0];
-
-    res.status(200).json({ post });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "something went wrong! sorry." });
-  }
+    try {
+        const queryResults = await postService.getPost(id);
+        const post = queryResults[0];
+        const previousPostQueryResult = await postService.getPreviousPost(post.id);
+        const previousPost = previousPostQueryResult[0];
+        res.status(200).json({ post, previousPost });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "something went wrong! sorry." });
+    }
 });
 
 postRouter.get("/draft/:id", async (req, res) => {
-  const {
-    params: { id },
-  } = req;
+    const {
+        params: { id },
+    } = req;
 
-  const postId = id;
+    const postId = id;
 
-  try {
-    const queryResults = await postService.getDraft(postId);
-    const post = queryResults[0];
+    try {
+        const queryResults = await postService.getDraft(postId);
+        const post = queryResults[0];
 
-    res.status(200).json({ post });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "something went wrong! sorry." });
-  }
+        res.status(200).json({ post });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "something went wrong! sorry." });
+    }
 });
 
 postRouter.post("/write", verifyToken, async (req, res) => {
-  const {
-    body: { title, post, type, status },
-    user: { sub },
-  } = req;
+    const {
+        body: { title, post, type, status },
+        user: { sub },
+    } = req;
 
-  try {
-    await postService.writePost(title, sub, post, type, status);
-    res.status(200).json({ message: "Upload success!" });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "something went wrong! sorry." });
-  }
+    try {
+        await postService.writePost(title, sub, post, type, status);
+        res.status(200).json({ message: "Upload success!" });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "something went wrong! sorry." });
+    }
 });
 
 postRouter.post("/edit", verifyToken, async (req, res) => {
-  const {
-    body: { title, post, postId, type, status },
-  } = req;
+    const {
+        body: { title, post, postId, type, status },
+    } = req;
 
-  try {
-    await postService.editPost(title, post, postId, type, status);
-    res.status(200).json({ message: "Edit success!" });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "something went wrong! sorry." });
-  }
+    try {
+        await postService.editPost(title, post, postId, type, status);
+        res.status(200).json({ message: "Edit success!" });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "something went wrong! sorry." });
+    }
 });
 
 postRouter.post("/delete", verifyToken, async (req, res) => {
-  const {
-    body: { postId },
-  } = req;
+    const {
+        body: { postId },
+    } = req;
 
-  try {
-    await postService.deletePost(postId);
-    res.status(200).json({ message: "Delete success!" });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "something went wrong! sorry." });
-  }
+    try {
+        await postService.deletePost(postId);
+        res.status(200).json({ message: "Delete success!" });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "something went wrong! sorry." });
+    }
 });
 
 module.exports = postRouter;
--- services/postService.js
+++ services/postService.js
@@ -1,103 +1,117 @@
 const query = require("../utils/query");
 
 class PostService {
-  getPost = async (postId) => {
-    try {
-      const queryString = `select A.id, A.post, A.created_at, A.title, B.email as author, A.author as author_id, B.deleted as deleted, A.type as type from posts A left join users B on A.author = B.id where A.id = ? and A.status = 0`;
-      const args = [postId];
-      const fn = async (conn) => {
-        const [rows] = await conn.query(queryString, args);
-        return rows;
-      };
-      return await query(fn);
-    } catch (err) {
-      throw new Error(err);
-    }
-  };
+    getPost = async (postId) => {
+        try {
+            const queryString = `select A.id, A.post, A.created_at, A.title, B.email as author, A.author as author_id, B.deleted as deleted, A.type as type from posts A left join users B on A.author = B.id where A.id = ? and A.status = 0`;
+            const args = [postId];
+            const fn = async (conn) => {
+                const [rows] = await conn.query(queryString, args);
+                return rows;
+            };
+            return await query(fn);
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
 
-  getPosts = async () => {
-    try {
-      const queryString = `select A.id, A.post, A.created_at, A.title, B.email as author, A.author as author_id, B.deleted as deleted, A.type as type from posts A left join users B on A.author = B.id where A.status = 0 order by A.created_at desc`;
-      const args = [];
-      const fn = async (conn) => {
-        const [rows] = await conn.query(queryString, args);
-        return rows;
-      };
-      return await query(fn);
-    } catch (err) {
-      throw new Error(err);
-    }
-  };
+    getPreviousPost = async (createdAt) => {
+        try {
+            const queryString = `select A.id, A.post, A.created_at, A.title, B.email as author, A.author as author_id, B.deleted as deleted, A.type as type from posts A left join users B on A.author = B.id where A.id < ? and A.status = 0 order by A.created_at desc limit 1`;
+            const args = [String(createdAt)];
+            const fn = async (conn) => {
+                const [rows] = await conn.query(queryString, args);
+                return rows;
+            };
+            return await query(fn);
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
 
-  getDraft = async (postId) => {
-    try {
-      const queryString = `select A.id, A.post, A.created_at, A.title, B.email as author, A.author as author_id, B.deleted as deleted, A.type as type from posts A left join users B on A.author = B.id where A.id = ?`;
-      const args = [postId];
-      const fn = async (conn) => {
-        const [rows] = await conn.query(queryString, args);
-        return rows;
-      };
-      return await query(fn);
-    } catch (err) {
-      throw new Error(err);
-    }
-  };
+    getPosts = async () => {
+        try {
+            const queryString = `select A.id, A.post, A.created_at, A.title, B.email as author, A.author as author_id, B.deleted as deleted, A.type as type from posts A left join users B on A.author = B.id where A.status = 0 order by A.created_at desc`;
+            const args = [];
+            const fn = async (conn) => {
+                const [rows] = await conn.query(queryString, args);
+                return rows;
+            };
+            return await query(fn);
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
 
-  getDrafts = async () => {
-    try {
-      const queryString = `select A.id, A.post, A.created_at, A.title, B.email as author, A.author as author_id, B.deleted as deleted, A.type as type from posts A left join users B on A.author = B.id where A.status > 0 order by A.created_at desc`;
-      const args = [];
-      const fn = async (conn) => {
-        const [rows] = await conn.query(queryString, args);
-        return rows;
-      };
-      return await query(fn);
-    } catch (err) {
-      throw new Error(err);
-    }
-  };
+    getDraft = async (postId) => {
+        try {
+            const queryString = `select A.id, A.post, A.created_at, A.title, B.email as author, A.author as author_id, B.deleted as deleted, A.type as type from posts A left join users B on A.author = B.id where A.id = ?`;
+            const args = [postId];
+            const fn = async (conn) => {
+                const [rows] = await conn.query(queryString, args);
+                return rows;
+            };
+            return await query(fn);
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
 
-  writePost = async (title, id, post, type, status) => {
-    try {
-      const queryString = `insert into posts (author, post, title, type, status, created_at) values (?, ?, ?, ?, ?, now())`;
-      const args = [id, post, title, type, status];
-      const fn = async (conn) => {
-        const [rows] = await conn.query(queryString, args);
-        return rows.insertId;
-      };
-      return await query(fn);
-    } catch (err) {
-      throw new Error(err);
-    }
-  };
+    getDrafts = async () => {
+        try {
+            const queryString = `select A.id, A.post, A.created_at, A.title, B.email as author, A.author as author_id, B.deleted as deleted, A.type as type from posts A left join users B on A.author = B.id where A.status > 0 order by A.created_at desc`;
+            const args = [];
+            const fn = async (conn) => {
+                const [rows] = await conn.query(queryString, args);
+                return rows;
+            };
+            return await query(fn);
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
 
-  editPost = async (title, post, postId, type, status) => {
-    try {
-      const queryString = `update posts set title = ?, post = ?, type = ?, status = ? where id = ?`;
-      const args = [title, post, type, status, postId];
-      const fn = async (conn) => {
-        const [rows] = await conn.query(queryString, args);
-        return rows.insertId;
-      };
-      return await query(fn);
-    } catch (err) {
-      throw new Error(err);
-    }
-  };
+    writePost = async (title, id, post, type, status) => {
+        try {
+            const queryString = `insert into posts (author, post, title, type, status, created_at) values (?, ?, ?, ?, ?, now())`;
+            const args = [id, post, title, type, status];
+            const fn = async (conn) => {
+                const [rows] = await conn.query(queryString, args);
+                return rows.insertId;
+            };
+            return await query(fn);
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
 
-  deletePost = async (postId) => {
-    try {
-      const queryString = `update posts set status = 1 where id = ?`;
-      const args = [postId];
-      const fn = async (conn) => {
-        const [rows] = await conn.query(queryString, args);
-        return rows.insertId;
-      };
-      return await query(fn);
-    } catch (err) {
-      throw new Error(err);
-    }
-  };
+    editPost = async (title, post, postId, type, status) => {
+        try {
+            const queryString = `update posts set title = ?, post = ?, type = ?, status = ? where id = ?`;
+            const args = [title, post, type, status, postId];
+            const fn = async (conn) => {
+                const [rows] = await conn.query(queryString, args);
+                return rows.insertId;
+            };
+            return await query(fn);
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
+
+    deletePost = async (postId) => {
+        try {
+            const queryString = `update posts set status = 1 where id = ?`;
+            const args = [postId];
+            const fn = async (conn) => {
+                const [rows] = await conn.query(queryString, args);
+                return rows.insertId;
+            };
+            return await query(fn);
+        } catch (err) {
+            throw new Error(err);
+        }
+    };
 }
 
 module.exports = PostService;
